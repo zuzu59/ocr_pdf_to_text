c258e57472fc9a51a19d50378d256678475df9c3
root <root@pdftotext.zuzu59.ch>
2024-11-12 16:12:13 +0100

    new: ajouté le pack français pour l'ocr et quelques utilitaires

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #Installation des outils pour faire de l'ocr sur des fichiers pdf
-#zf241112.1502
+#zf241112.1528
 
 sudo apt-get update
-sudo apt-get install poppler-utils tesseract-ocr
+sudo apt-get install poppler-utils tesseract-ocr tesseract-ocr-fra htop tree
 
